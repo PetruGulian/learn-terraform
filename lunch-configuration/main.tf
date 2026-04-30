9f1b91a14623fd30dc6e389390c5e4a3a8a6d307
Petru Gulian <petregmd@gmail.com>
2026-04-30 16:42:11 +0100

    Changing the configuration to use the lunch_template instead of lunch_configuration because of the following error: Error: creating Auto Scaling Launch Configuration (terraform-20260430103928539400000001): operation error Auto Scaling: CreateLaunchConfiguration, https response error StatusCode: 400, RequestID: 632a6054-6dad-4a93-b8b5-a748d8b4a252, api error UnsupportedOperation: The Launch Configuration creation operation is not available in your account. Use launch templates to create configuration templates for your Auto Scaling groups. │ │   with aws_launch_configuration.example, │   on main.tf line 5, in resource aws_launch_configuration example: │    5: resource aws_launch_configuration example {

--- lunch-configuration/main.tf
+++ lunch-configuration/main.tf
@@ -1,29 +1,113 @@
+# provider "aws" {
+#   region = "us-east-2"
+# }
+
+# resource "aws_launch_configuration" "example" {
+#   image_id        = "ami-0fb653ca2d3203ac1"
+#   instance_type   = "t2.micro"
+#   security_groups = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#           #!/bin/bash
+#           echo "Hello, Gulian Technology!" > index.html
+#           nohup busybox httpd -f -p ${var.server_port} &
+#           EOF
+#   # Required when using a launch configuration with an auto scaling group.
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+# resource "aws_autoscaling_group" "example" {
+#   launch_configuration = aws_launch_configuration.example.name
+#   vpc_zone_identifier  = data.aws_subnets.default.ids
+
+#   min_size = 2
+#   max_size = 10
+#   tag {
+#     key                 = "Name"
+#     value               = "terraform-asg-example"
+#     propagate_at_launch = true
+#   }
+# }
+
+# resource "aws_security_group" "instance" {
+#   name = "terraform-example-instance"
+#   ingress {
+#     from_port   = var.server_port
+#     to_port     = var.server_port
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+# }
+
+# data "aws_vpc" "default" {
+#   default = true
+# }
+
+# data "aws_subnets" "default" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.default.id]
+#   }
+# }
+
+
 provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+# # ---------------------------------------------------------------------------
+# # Variable – server port used by the HTTP server and the security group rule
+# # ---------------------------------------------------------------------------
+# variable "server_port" {
+#   description = "The port the web server will listen on"
+#   type        = number
+#   default     = 8080
+# }
 
-  user_data = <<-EOF
-          #!/bin/bash
-          echo "Hello, Gulian Technology!" > index.html
-          nohup busybox httpd -f -p ${var.server_port} &
-          EOF
-  # Required when using a launch configuration with an auto scaling group.
+# ---------------------------------------------------------------------------
+# Launch Template  (replaces the deprecated aws_launch_configuration)
+# ---------------------------------------------------------------------------
+resource "aws_launch_template" "example" {
+  name_prefix   = "terraform-lt-example-"
+  image_id      = "ami-0fb653ca2d3203ac1"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(<<-EOF
+    #!/bin/bash
+    echo "Hello, Gulian Technology!" > index.html
+    nohup busybox httpd -f -p ${var.server_port} &
+    EOF
+  )
+
+  # Ensures a new template version is created before the old one is destroyed,
+  # which is the equivalent of the lifecycle block that was on the launch
+  # configuration.
   lifecycle {
     create_before_destroy = true
   }
 }
 
+# ---------------------------------------------------------------------------
+# Auto Scaling Group
+# ---------------------------------------------------------------------------
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnets.default.ids
 
   min_size = 2
   max_size = 10
+
+  # Launch templates are referenced via a nested block rather than a plain
+  # name attribute.
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
   tag {
     key                 = "Name"
     value               = "terraform-asg-example"
@@ -31,17 +115,23 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
+# ---------------------------------------------------------------------------
+# Security Group
+# ---------------------------------------------------------------------------
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 }
 
+# ---------------------------------------------------------------------------
+# Data Sources
+# ---------------------------------------------------------------------------
 data "aws_vpc" "default" {
   default = true
 }
